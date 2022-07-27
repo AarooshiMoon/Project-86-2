d778757bd73205e001f0dd5d07a60c75a9d5aec7
AarooshiMoon <77153634+AarooshiMoon@users.noreply.github.com>
2022-07-27 18:02:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2QLb7lXKWnUeLGXmleSDInl4dzanQqOQ",
+  authDomain: "spectagram-app-8b7cd.firebaseapp.com",
+  databaseURL: "https://spectagram-app-8b7cd-default-rtdb.firebaseio.com",
+  projectId: "spectagram-app-8b7cd",
+  storageBucket: "spectagram-app-8b7cd.appspot.com",
+  messagingSenderId: "839537008445",
+  appId: "1:839537008445:web:a8d0a8a4f7caa5942d6f54"
+};
